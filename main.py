f293cf9d71511f8b4190d9b7a2990908c5ba3e57
xCALibUR21ATN <120234330+xCALibUR21ATN@users.noreply.github.com>
2025-10-01 22:13:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/process-images', methods=['POST'])
+def process_images():
+    if 'image1' not in request.files or 'image2' not in request.files:
+        return jsonify({"error": "Both image1 and image2 must be uploaded"}), 400
+
+    image1 = request.files['image1']
+    image2 = request.files['image2']
+
+    filename1 = secure_filename(image1.filename)
+    filename2 = secure_filename(image2.filename)
+
+    # Here you can add your image processing code with cv2, skimage, etc.
+
+    # For now, just return the filenames as a demonstration
+    return jsonify({
+        "image1_name": filename1,
+        "image2_name": filename2,
+        "message": "Images received and processed"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
